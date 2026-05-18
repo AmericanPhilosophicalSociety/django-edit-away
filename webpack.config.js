78da19a718382f2d882fc92c26b67802246d443d
David Ragnar Nelson <drnelson6@gmail.com>
2026-05-18 14:03:12 -0400

    Webpack build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './assets/index.js',
+    output: {
+        filename: 'tiptap.js',
+        path: path.resolve(__dirname, './src/django_exhibits/static/js'),
+        library: 'djangoEditor',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+};
