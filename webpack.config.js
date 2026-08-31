57c3946638687201b6aa0b1e2eaf9db5b5f132a4
David Ragnar Nelson <drnelson6@gmail.com>
2026-08-31 14:22:07 -0400

    Fix path on webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ export default {
     entry: './assets/index.js',
     output: {
         filename: 'tiptap.js',
-        path: path.resolve(__dirname, './src/django_exhibits/static/django_exhibits'),
+        path: path.resolve(__dirname, './src/django_edit_away/static/django_edit_away'),
         library: 'djangoEditor',
     },
     module: {
